77422b9b747217abb0d55a7cf280e25c98dee3e2
Arthur Correnson <arthur.correnson@gmail.com>
2019-09-26 22:14:01 +0200

    ajoute une meilleure definition de mod2

--- LOGIC/entiers_naturels/nat.v
+++ LOGIC/entiers_naturels/nat.v
@@ -1,9 +1,16 @@
+(* --------------------- *)
+(* -- Inductive Nat ---- *)
+(* --------------------- *)
+
 Inductive nat : Set :=
   | O : nat
   | S : nat -> nat.
 
 
-(* Minimalistic version of addition *)
+(* --------------------- *)
+(* ----- Addition ------ *)
+(* --------------------- *)
+
 Fixpoint plus (n m:nat) : nat :=
   match n with
   | O   => m
@@ -11,7 +18,10 @@ Fixpoint plus (n m:nat) : nat :=
   end.
 
 
-(* Multiplication *)
+(* --------------------- *)
+(* -- Multiplication --- *)
+(* --------------------- *)
+
 Fixpoint mult(n m:nat) : nat :=
   match n with
   | O => O
@@ -19,5 +29,36 @@ Fixpoint mult(n m:nat) : nat :=
   end.
 
 
+(* --------------------- *)
+(* --- Modulo 2 -------- *)
+(* --------------------- *)
+
+Fixpoint mod2(n:nat) : nat :=
+  match n with
+  | O   => O
+  | S p =>  match p with
+            | O => S O
+            | S q => mod2 q
+            end
+
+  end.
+
+
+(* --------------------- *)
+(* ------- TESTS ------- *)
+(* --------------------- *)
+
+(* Should outputs (S (S O)) *)
 Eval compute in (plus (S O) (S O)).
+
+(* Should outputs (S (S (S (S O)))) *)
 Eval compute in (mult (S (S O)) (S (S O))).
+
+(* Should outputs O *)
+Eval compute in (mod2 (S (S (S (S O))))).
+
+(* Should outputs S O *)
+Eval compute in (mod2 (S (S (S O)))).
+
+(* Should outputs O *)
+Eval compute in (mod2 ((S O))).
