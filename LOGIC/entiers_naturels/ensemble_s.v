722599d298f03074def14ce0f88f06863ceef678
Arthur Correnson <arthur.correnson@gmail.com>
2019-09-29 14:03:43 +0200

    Delete ensemble_s.v

--- LOGIC/entiers_naturels/ensemble_s.v
+++
@@ -1,8 +0,0 @@
-(* ------------------------------------ *)
-(* exercice sur les defnition inductive *)
-(* ------------------------------------ *)
-Require Import Arith.
-
-Inductive s : Set :=
-  | quatre
-  | trois.
