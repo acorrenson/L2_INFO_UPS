8bebdc27145d27822d19ff91ff075505e81ec579
Arthur Correnson <arthur.correnson@gmail.com>
2020-02-13 16:33:27 +0100

    Add stack implementation

---
+++ SDD/stack/stack.c
@@ -0,0 +1,27 @@
+#include "stack.h"
+
+void stack_create(stack_t *s) { s->top = -1; }
+
+bool stack_is_empty(stack_t *s) { return s->top == -1; };
+
+void stack_push(stack_t *s, int e) {
+  assert(s->top < MAX_SIZE - 1);
+  ++s->top;
+  s->stack_data[s->top] = e;
+}
+
+void stack_pop(stack_t *s) {
+  assert(s->top != -1);
+  --s->top;
+}
+
+int main(int argc, char const *argv[]) {
+  stack_t s;
+  stack_create(&s);
+  assert(stack_is_empty(&s));
+  stack_push(&s, 1);
+  stack_pop(&s);
+  assert(stack_is_empty(&s));
+
+  return 0;
+}
---
+++ SDD/stack/stack.h
@@ -0,0 +1,58 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <assert.h>
+#include <stdbool.h>
+
+#define MAX_SIZE 256
+
+/**
+ * @brief Stack Structure.
+ *
+ */
+typedef struct stack_s {
+  int stack_data[MAX_SIZE];
+  int top;
+} stack_t;
+
+/**
+ * @brief Stack constructor stack_create.
+ *
+ * @semantic default constructor
+ *
+ */
+void stack_create(stack_t *);
+
+/**
+ * @brief Stack operator stack_push.
+ *
+ * @pre
+ *  s.top < MAX_SIZE - 1
+ * @semantic
+ *  default constructor
+ *
+ */
+void stack_push(stack_t *, int);
+
+/**
+ * @brief Stack operator stack_create.
+ *
+ * @semantic
+ *  is_empty(create())
+ *  ¬is_empty(push(s))
+ *
+ */
+bool stack_is_empty(stack_t *);
+
+/**
+ * @brief Stack operator stack_pop
+ *
+ * @pre
+ *  ¬is_empty(s)
+ * @semantic
+ *  pop(push(s, x)) = s
+ *
+ */
+void stack_pop(stack_t *);
+
+#endif
